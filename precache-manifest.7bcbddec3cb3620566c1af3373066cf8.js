0210448fe1cee213c98bddd99e50ef560886684a
vijay <o.vijaykumarn@gmail.com>
2019-05-19 19:43:56 +0530

    Updates

---
+++ precache-manifest.7bcbddec3cb3620566c1af3373066cf8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8ff44d3b19ce73fac1",
+    "url": "/react-redux-practise/static/js/runtime~main.7ebadfbb.js"
+  },
+  {
+    "revision": "57184d597f98521f0396",
+    "url": "/react-redux-practise/static/js/main.7202a820.chunk.js"
+  },
+  {
+    "revision": "004bc6740d22f554b41d",
+    "url": "/react-redux-practise/static/js/2.50096723.chunk.js"
+  },
+  {
+    "revision": "61781f64a0116ee0b84ff5595c3f65db",
+    "url": "/react-redux-practise/index.html"
+  }
+];
